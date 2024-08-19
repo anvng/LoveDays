925239caa3128891adfe4bb50b802779ffd55e3e
An Van Nguyen <152575252+anvng@users.noreply.github.com>
2024-08-20 00:30:11 +0700

    feat: update and edit person infor

--- lib/model/person_model.dart
+++ lib/model/person_model.dart
@@ -3,6 +3,7 @@ import '../utils/date_utils.dart';
 class Person {
   late final String name;
   late final DateTime dateOfBirth;
+  String? get profileImage => null;
 
   String get zodiacSign => getZodiacSign(dateOfBirth);
   int get age => getCurrentAge(dateOfBirth);
@@ -11,6 +12,7 @@ class Person {
   Person({
     required this.name,
     required this.dateOfBirth,
+    String? profileImage,
   });
 
   get imageName => null;
