00898d7d7ad3cdeb45377ef31b2b3dd13bb685f9
An Van Nguyen <152575252+anvng@users.noreply.github.com>
2024-08-20 17:04:30 +0700

    style: add icons and quotes

--- lib/data/quotes.dart
+++ lib/data/quotes.dart
@@ -5,5 +5,14 @@ final List<String> loveQuotes = [
   "Love is when the other person's happiness is more important than your own. – H. Jackson Brown, Jr.",
   "In all the world, there is no heart for me like yours. – Maya Angelou",
   "You are my heart, my life, my one and only thought. – Arthur Conan Doyle",
-  // Add more quotes here
+  '"Every morning when I wake up, the most beautiful flower in my heart is your smile!" - ăn đép',
+  '"I fell in love with her courage, her sincerity, and her flaming self respect. And it’s these things I’d believe in, even if the whole world indulged in wild suspicions that she wasn’t all she should be. I love her and it is the beginning of everything.” – F. Scott Fitzgerald',
+  '"I love you. I have loved you since the first moment I saw you. I guess maybe I have even loved you before I saw you." — A Place in the Sun',
+  '"I would have really liked just doing laundry and taxes with you." — Everything Everywhere All At Once',
+  '"If I know what love is, it is because of you." — Herman Hesse, Narcissus and Goldmund',
+  '"If one day the moon calls you by your name do not be surprised, because every night I tell her about you." — Shahrazad al-Khalij',
+  '"Deep within us—no matter who we are—there lives a feeling of wanting to be lovable, of wanting to be the kind of person that others like to be with. And the greatest thing we can do is to let people know that they are loved and capable of loving." — Fred Rogers',
+  '"It is you. It is me. It is us." — The Haunting of Bly Manor',
+  '"Nobody has ever measured, not even poets, how much the heart can hold." — Zelda Fitzgerald',
+  '"こうふく" - anvndev',
 ];
